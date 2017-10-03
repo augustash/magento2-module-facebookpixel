2b0d4c26070113e950c01c246a039df12129bce2
kazajhodo <acornell@augustash.com>
2017-10-03 13:17:57 -0500

    Various modifications
    Currently saves and retreives basic data properly, encountering odd class not found error. Unresolved at this point.
    
    “Fatal error: Class 'Augustash\Facebookpixel\Block\Snippet' not found”

--- Block/Snippet.php
+++ Block/Snippet.php
@@ -15,10 +15,6 @@ class Snippet extends \Magento\Framework\View\Element\Template
      */
     protected $accountNumber;
 
-    protected $baseUrl = '//script.crazyegg.com/pages/scripts/';
-    protected $baseUrlSuffix = '.js?';
-
-
     /**
      * Retrieve configuration value for `enabled`
      *
@@ -27,7 +23,7 @@ class Snippet extends \Magento\Framework\View\Element\Template
     public function getIsEnabled()
     {
         $this->isEnabled = $this->_scopeConfig->getValue(
-            'augustash_crazyegg/general/enabled',
+            'augustash_facebookpixel/general/enabled',
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE
         );
         return $this->isEnabled;
@@ -40,33 +36,11 @@ class Snippet extends \Magento\Framework\View\Element\Template
      */
     public function getAccountNumber()
     {
-        if (empty($this->accountNumber)) {
-            $this->accountNumber = $this->_scopeConfig->getValue(
-                'augustash_crazyegg/general/account_number',
-                \Magento\Store\Model\ScopeInterface::SCOPE_STORE
-            );
-        }
+        $this->accountNumber = $this->_scopeConfig->getValue(
+            'augustash_facebookpixel/general/account_number',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+
         return $this->accountNumber;
-    }
-
-    /**
-     * takes the $baseUrl and splits the account number
-     * to create a URL similar to this:
-     *
-     *   //script.crazyegg.com/pages/scripts/0001/1234.js?
-     *
-     * @return string
-     */
-    public function getJavascriptUrl()
-    {
-        $accountNumber = $this->getAccountNumber();
-        if ($accountNumber) {
-            $part1 = substr($accountNumber, 0, 4);
-            $part2 = substr($accountNumber, 4, 4);
-
-            return $this->baseUrl . $part1 . '/' . $part2 . $this->baseUrlSuffix;
-        } else {
-            return $this->baseUrl . 'MISSING/ACCT#' . $this->baseUrlSuffix;
-        }
     }
 }
