6c92c0c6fdce2da5a543a2f5bc62cf69573f7bf6
kazajhodo <acornell@augustash.com>
2017-10-31 16:18:38 -0500

    Added Revenue tracking
    Tracking can be turned on within the admin options under the advanced section.
    Enabling revenue tracking will return order totals to the Facebook pixel dashboard for us in analytics.

---
+++ Block/Revenue.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Augustash\Facebookpixel\Block;
+
+use Magento\Checkout\Block\Onepage\Success;
+use Augustash\Facebookpixel\Block\Snippet as SnippetHelper;
+use Psr\Log\LoggerInterface as LoggerData;
+
+/**
+ * For logging arrays
+ * $this->helper->addWarning('Data: ', $output);
+ */
+
+/**
+ * One page checkout success page
+ */
+class Revenue extends \Magento\Framework\View\Element\Template
+{
+	/**
+     * @var boolean
+     */
+    protected $revenueEnabled;
+
+	/**
+     * @var \Magento\Checkout\Model\Session
+     */
+	protected $checkoutSession;
+
+	public function __construct(
+	        \Magento\Framework\View\Element\Template\Context $context,
+	        \Magento\Checkout\Model\Session $checkoutSession,
+	        SnippetHelper $snippet,
+	        LoggerData $helper,
+	        array $data = []
+	    )
+	{
+		$this->helper = $helper;
+		$this->snippet = $snippet;
+	    $this->checkoutSession = $checkoutSession;
+
+        parent::__construct($context, $data);
+	}
+
+	/**
+	 *	Retrieve last total amount from checkout session
+	 *
+     * @return string
+     */
+	public function getRevenue()
+	{
+		$order = $this->checkoutSession->getLastRealOrder();
+
+		return $order->getData('total_due');
+    }
+
+    /**
+     * Check that revenue tracking is enabled in admin
+     *
+     * @return boolean
+     */
+    public function getTrackRevenue()
+    {
+    	$this->revenueEnabled = $this->_scopeConfig->getValue(
+            'augustash_facebookpixel/advanced/revenue',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+
+        return $this->revenueEnabled;
+    }
+}
